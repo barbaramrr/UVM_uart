4bfc98f021f67fc3f2b530c2a7dcdd9eebca7aaa
barbaramrr <86504723+barbaramrr@users.noreply.github.com>
2026-04-16 17:27:45 -0300

    alteração para baud

--- sim/random_sequence.sv
+++ sim/random_sequence.sv
@@ -25,9 +25,8 @@ class random_sequence extends uvm_sequence #(uart_item);
                 `uvm_error(get_full_name(), "Failed to get random uart_item")
             end
             item_count++;
+            `uvm_info(get_full_name(), $sformatf("Sequence item %d: Generated random baud code: %d", item_count, seq_item.baud), UVM_MEDIUM)
             `uvm_info(get_full_name(), $sformatf("Sequence item %d: Generated random data: 0x%02h", item_count, seq_item.data), UVM_MEDIUM)
-             uvm_config_db#(bit [4:0])::set(null, "*", "CURRENT_BAUD", seq_item.baud);
-             `uvm_info(get_full_name(), $sformatf("Sequence item %d: Generated random baud: 0x%02d", item_count, seq_item.baud), UVM_MEDIUM)
             finish_item(seq_item); 
         end
     endtask : body
