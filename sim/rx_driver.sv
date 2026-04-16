2333b5b8c9f48164512e52b37d32773908eb3457
barbaramrr <86504723+barbaramrr@users.noreply.github.com>
2026-04-16 17:27:05 -0300

    Implement baud rate conversion function
    Added baud rate conversion function to translate enumeration to real values in bps.

--- sim/rx_driver.sv
+++ sim/rx_driver.sv
@@ -20,6 +20,43 @@ class rx_driver extends uvm_driver #(uart_item);
         if (!uvm_config_db#(virtual uart_bfm)::get(this, "", "bfm_uart0", bfm_uart0))
             `uvm_fatal(get_full_name(), "BFM not set via uvm_config_db");
     endfunction : build_phase
+// Alteração: Função de conversão de enumeração de baud rate para valor real (em bps)
+    function real baud_conv (input bit [4:0] baud_enum);
+        case (baud_enum)
+            5'd0 : baud_conv = 50.0;
+            5'd1 : baud_conv = 75.0;
+            5'd2 : baud_conv = 110.0;
+            5'd3 : baud_conv = 134.0;
+            5'd4 : baud_conv = 150.0;
+            5'd5 : baud_conv = 200.0;
+            5'd6 : baud_conv = 300.0;
+            5'd7 : baud_conv = 600.0;
+            5'd8 : baud_conv = 1200.0;
+            5'd9 : baud_conv = 1800.0;
+            5'd10: baud_conv = 2400.0;
+            5'd11: baud_conv = 4800.0;
+            5'd12: baud_conv = 9600.0;
+            5'd13: baud_conv = 14400.0;
+            5'd14: baud_conv = 19200.0;
+            5'd15: baud_conv = 28800.0;
+            5'd16: baud_conv = 31250.0;
+            5'd17: baud_conv = 38400.0;
+            5'd18: baud_conv = 56000.0;
+            5'd19: baud_conv = 57600.0;
+            5'd20: baud_conv = 76800.0;
+            5'd21: baud_conv = 115200.0;
+            5'd22: baud_conv = 128000.0;
+            5'd23: baud_conv = 153600.0;
+            5'd24: baud_conv = 230400.0;
+            5'd25: baud_conv = 256000.0;
+            5'd26: baud_conv = 460800.0;
+            5'd27: baud_conv = 500000.0;
+            5'd28: baud_conv = 576000.0;
+            5'd29: baud_conv = 921600.0;
+            default: baud_conv = 115200.0; // Valor padrão
+        endcase
+        
+    endfunction
 
     task run_phase(uvm_phase phase);
         uart_item command;
@@ -31,7 +68,9 @@ class rx_driver extends uvm_driver #(uart_item);
 
         forever begin
             seq_item_port.get_next_item(command);
-            bfm_uart0.send(command.data);
+            `uvm_info(get_full_name(), $sformatf("Enviando bit-stream para RX da DUT. Baud: %0f", 
+                      baud_conv(command.baud)), UVM_MEDIUM)
+            bfm_uart0.send(command.data, 8, baud_conv(command.baud), "none", 0); // Alteração: Passa a velocidade convertida para o BFM
             seq_item_port.item_done();
         end
     endtask : run_phase
