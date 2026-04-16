6c6bd6503fa962b879820f340c884183f9c9ec06
barbaramrr <86504723+barbaramrr@users.noreply.github.com>
2026-04-16 17:28:46 -0300

    Update uart_item.sv

--- sim/uart_item.sv
+++ sim/uart_item.sv
@@ -4,14 +4,14 @@
 
 import uvm_pkg::*;
 `include "uvm_macros.svh"
+//`include "baud_rate_type.svh" // Inclui a definição do tipo de baud rate
 
 class uart_item extends uvm_sequence_item;
     `uvm_object_utils(uart_item)
 
-
     rand bit [7:0] data;
-    rand uart_baud_rate_t baud;
-    // 
+   // rand uart_baud_rate_t baud; // Usa o tipo de baud rate definido em baud_rate_type.sv
+    rand bit [4:0] baud; // Enumeração de baud rate (5 bits para representar até 32 valores)
 
     function new(string name = "uart_item");
         super.new(name);
